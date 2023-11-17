1bbc891796580d8bd09b429c784c9ea8d1c47481
Capti <captianbeau@gmail.com>
2023-11-16 22:25:10 -0700

    added code

--- script.js
+++ script.js
@@ -9,7 +9,10 @@ $(function () {
 var currentTime = dayjs().format("HH")
 console.log(currentTime)
 saveBtn.on('click') 
-});
+if(notice.matches("hour-9")){
+    setLocalStorage("9AM",)
+}
+
 
 // class= past,present or future  id = hour-9
 // only one present at a time
